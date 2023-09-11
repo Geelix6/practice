be72157ba51c56a3d7933295140a2c01a33b251f
Geelix6 <geelix@mail.ru>
2023-09-11 11:45:14 +0300

    Написан скрипт меняющий цвет заднего фона

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("JavaScript is working!");
+
+function generateRandomColor() {
+  const hexCodes = "0123456789abcdef";
+  let color = "";
+  for (let _ = 0; _ < 6; _++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return "#" + color;
+}
+
+setInterval(() => (document.body.style.backgroundColor = generateRandomColor()), 1000);
